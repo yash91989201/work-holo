89f1fe2ed7a6e6a209b035998df68a2680709e16
Yashraj Jaiswal <yashraj6370@gmail.com>
2026-03-08 23:35:20 +0530

    feat(api): pass replyToMessageId in dm.sendMessage handler

--- packages/api/src/routers/communication/dm.ts
+++ packages/api/src/routers/communication/dm.ts
@@ -392,6 +392,7 @@ export const dmRouter = {
             content: normalizedContent,
             type: input.type,
             parentMessageId: input.parentMessageId,
+            replyToMessageId: input.replyToMessageId,
           })
           .returning();
 
