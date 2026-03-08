c57c06fcfcd2f48c4596feb5acbf926b31e55dca
Yashraj Jaiswal <yashraj6370@gmail.com>
2026-03-08 23:35:05 +0530

    feat(api): pass replyToMessageId in message.create handler

--- packages/api/src/routers/communication/message.ts
+++ packages/api/src/routers/communication/message.ts
@@ -161,6 +161,7 @@ export const messageRouter = {
             content: input.content,
             type: input.type,
             parentMessageId: input.parentMessageId,
+            replyToMessageId: input.replyToMessageId,
             senderId: user.id,
           })
           .returning();
