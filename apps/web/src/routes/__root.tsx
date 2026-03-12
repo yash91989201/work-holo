454e591bb51f98dd824486ad52e6b1f8b235a2f4
Yashraj Jaiswal <yashraj6370@gmail.com>
2026-03-12 11:39:25 +0530

    Change default theme from dark to light

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -45,7 +45,7 @@ function RootComponent() {
       <HeadContent />
       <ThemeProvider
         attribute="class"
-        defaultTheme="dark"
+        defaultTheme="light"
         disableTransitionOnChange
         storageKey="vite-ui-theme"
       >
